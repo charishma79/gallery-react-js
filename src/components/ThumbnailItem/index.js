68fdb9b7f1a3d8a2afcd9eadd9f4e910c7fcbf93
charishma79 <122459991+charishma79@users.noreply.github.com>
2023-02-02 21:41:53 +0530

    updated buttonstyle

--- src/components/ThumbnailItem/index.js
+++ src/components/ThumbnailItem/index.js
@@ -2,17 +2,21 @@
 import './index.css'
 
 const ThumbnailItem = props => {
-  const {imageDetails, updateActiveImage, updateAlt} = props
+  const {imageDetails, updateActiveImage, updateAlt, isActive} = props
   const {thumbnailUrl, thumbnailAltText, imageUrl, imageAltText} = imageDetails
 
   const onClickThumbnail = () => {
     updateActiveImage(imageUrl)
     updateAlt(imageAltText)
   }
-
+  const activeThumbnailName = isActive ? '' : 'btn'
   return (
     <li className="images-folder">
-      <button className="button" type="button" onClick={onClickThumbnail}>
+      <button
+        className={`button ${activeThumbnailName}`}
+        type="button"
+        onClick={onClickThumbnail}
+      >
         <img src={thumbnailUrl} className="image" alt={thumbnailAltText} />
       </button>
     </li>
